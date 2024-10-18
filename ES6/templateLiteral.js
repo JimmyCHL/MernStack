08429795a4f443d86b9a9deb514e5d11fe010087
JimmyCHL <a0930795105@gmail.com>
2024-10-18 12:26:53 -0400

    add new content for export_import, shorthand and templateLiteral

---
+++ ES6/templateLiteral.js
@@ -0,0 +1,43 @@
+// Template literal in ES6 is used to create a better implementation of strings, html, json data
+// without the need of string separaters, html identifiers and new lines
+
+// `` - use back tick or tild operator and write your string, html, variables etc it will return the data in same structure
+// to use objects/variables we should use ${---here the variables---}
+
+let dynVal = 2024
+let AnimalSoundsES6 = require('./shorthand')
+
+let myNormalString =
+  'Lorem Lipsum Lorem Lipsum Lorem Lipsum' +
+  '\n Lorem Lipsum Lorem Lipsum ' +
+  dynVal +
+  ' Lorem Lipsum' +
+  '\n Lorem Lipsum Lorem Lipsum Lorem Lipsum' +
+  '\n Lorem Lipsum Lorem Lipsum Lorem Lipsum' +
+  '<h1>Header</h1>' +
+  '\n Lorem Lipsum Lorem Lipsum Lorem Lipsum \n' +
+  JSON.stringify(AnimalSoundsES6)
+
+console.log(myNormalString)
+
+let myTemplateLiteral = `Lorem Lipsum Lorem Lipsum Lorem Lipsum
+Lorem Lipsum Lorem Lipsum ${dynVal} Lorem Lipsum
+        Lorem Lipsum Lorem Lipsum Lorem Lipsum
+Lorem Lipsum Lorem Lipsum Lorem Lipsum
+                    <h1>Header</h1>
+Lorem Lipsum Lorem Lipsum Lorem Lipsum
+
+${JSON.stringify(AnimalSoundsES6)}`
+console.log(myTemplateLiteral)
+
+//exercise : write details about yourself using template literal and use variable and objects to show the value
+
+const jimmyInfo = { name: 'Jimmy', age: 30, state: 'SC', country: 'USA' }
+
+const getAge = (obj) => obj.age
+
+let myInfo = `My name is ${jimmyInfo.name} and I am ${getAge(jimmyInfo)} years old. Also I am living in ${
+  jimmyInfo.state
+}, ${jimmyInfo.country}`
+
+console.log(myInfo)
