67c41b089ed5d50eb6a7018f74e71a34a66ed97c
JimmyCHL <a0930795105@gmail.com>
2024-10-30 11:35:41 -0400

    node rest_api knowledge

--- ES6/rest_api.js
+++ ES6/rest_api.js
@@ -2,3 +2,28 @@
 //http - representation data is the data transfered via http request/response
 //http -> stateless
 //state -
+
+//Service Oriented Architecture - SOA
+//Implements - Separation of Concerns
+//<backend (Service, API, Endpoints)>
+// <frontend (html, css, js and its frameworks - angular, react, ...etc with SPA)
+
+// Restful API - Standards or Specifications
+
+//TCP/IP and WCF or IMAP etc - HTTP => all rest api's must have HTTP/s protocol
+
+// It should follow http verbs or CRUD operations
+
+// POST => Save/Create data in server (create)
+// GET/GetAll => Retrieve data from server (read)
+// PUT/Patch => Update data in server (update)
+// DELETE => Delete data from server basis of id passed (delete)
+
+// Put - updates entire record (each field and the value) + id
+// Patch - updates the specific field of the record (not each field) + id
+
+// www.learnmern.edu/api/user
+// Get/Get by Id
+// Post - data in payload
+// Put/Patch - data in payload
+// Delete - with id
