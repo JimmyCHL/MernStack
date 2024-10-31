d7f9c825a27d80054148f12c9470fb8245c5aab9
JimmyCHL <a0930795105@gmail.com>
2024-10-31 12:15:11 -0400

    nodemon and learning package and packagelock

---
+++ node_api/api.js
@@ -0,0 +1,41 @@
+//API - Application Programming Interface
+
+//Restful
+//http - <vers, request, response>
+//let http = require('http')
+//http.get()
+
+//cookies, sessions, query string, route param
+//json - data type, size - of data passed
+
+// re-inventing the wheel <when we do everything together>
+
+// express - a complete package for building the useful application
+
+// express - package is present on npm.js .org
+
+// command line tools to build node applications
+// npm - node package manager tool is used to create setup
+// yarn - can also be used to create node application
+
+// commands used
+
+// npm - init < initialize the application> <package.json - is created>
+
+// to install the package
+
+// npm install <package name>
+// npm i <package name>
+
+// once installed it adds to the dependency field
+// also creates a new folder with name <node_modules>
+
+// npm or yarn commands are registered in package.json file under scripts field
+
+// npm install <package name> will be dependencies.
+// npm install -D <package name> will be devDependencies.
+// yarn add <package name> will be dependencies.
+// yarn add -D <package name> will be devDependencies.
+
+// 'start' command in the script is a reserved command, so you cna start the application using npm start / yarn start
+// For other custom command, you can use npm run <command> / yarn run <command> / yarn <command>
