fb0f86840ff37b5aac4ec59eb9f1b9568f639932
JimmyCHL <a0930795105@gmail.com>
2024-11-08 11:41:20 -0500

    still in setup process

--- reactApp/src/application/app.js
+++ reactApp/src/application/app.js
@@ -1,6 +1,25 @@
 import React from 'react'
 export default class Application extends React.Component {
   render() {
-    return <h2>This is my first react page from application!! </h2>
+    let myName = 'Windiee - JSX => Javascript Like XML Structure'
+    let nameList = ['Nilay', 'Gesan', 'Jimmy', 'Ben Ma', 'Jay']
+    let nameListWorking = ['Windie', 'Stacy', 'Duncan']
+
+    return (
+      <div>
+        <h2>This is my first react page from application!! </h2>
+        <h2>This is my first react page from application!! </h2>
+        <h2>This is my first react page from application!! </h2>
+        <p1>{myName}</p1>
+        <h3>Names List</h3>
+        {nameList.map((name, index) => {
+          return <p key={index}>{name}</p>
+        })}
+        <h3>Names List Working</h3>
+        {nameListWorking.map((name, index) => {
+          return <p key={index}>{name}</p>
+        })}
+      </div>
+    )
   }
 }
