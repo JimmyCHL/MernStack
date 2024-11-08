7e99b36895d97885ceeb0a6e19e780a09b61ee60
JimmyCHL <a0930795105@gmail.com>
2024-11-07 21:35:53 -0500

    configure first webpack babel react configuration with yarn

--- ES6/spa.js
+++ ES6/spa.js
@@ -33,3 +33,34 @@
 // Hosting the frontend
 // We can host via express, webpack is better solution as it can do all other above tasks like
 // transpilation, bundling, minification, compression, split etc... along with giving host from node.
+
+// - babel-loader
+/**
+ * A loader for Webpack that tells Webpack to use Babel for transpiling JavaScript files.
+ * It acts as a bridge between Webpack and Babel,
+ * ensuring that all JS files are processed by Babel before being bundled by Webpack.
+ */
+
+// - html-webpack-plugin
+/**
+ * A Webpack plugin that simplifies the creation of an HTML file to include the bundled assets.
+ * This plugin injects the necessary script and link tags into an HTML file,
+ * making it easy to load the bundled JavaScript and CSS.
+ */
+
+// "devDependencies": {
+//     "@babel/core": "^7.23.6", // manage the babel transformation process
+//     "@babel/preset-env": "^7.23.6", //converting es6 to core js
+//     "@babel/preset-react": "^7.23.3", //converting react syntax to core js
+//     "babel-loader": "^9.1.3",
+//     "css-loader": "^6.8.1", //helper library to convert the css into js  bundle
+//     "html-webpack-plugin": "^5.5.4", //helps to launch our html page with necessary links for css or javascript while serving the request
+//     "style-loader": "^3.3.3", // helps load css into html <style> tag
+//     "webpack": "^5.89.0", //bundling, task runner, loader, configuration runner, bower runner
+//     "webpack-cli": "^5.1.4", // to run web pack command like - serve, build etc
+//     "webpack-dev-server": "^4.15.1" // front end application host
+//   },
+
+//npm i -D @babel/core @babel/preset-env @babel/preset-react html-webpack-plugin style-loader webpack-cli webpack-dev-server
+//copy the package.json
+//run npm i
---
+++ reactApp/src/application/app.js
@@ -0,0 +1,6 @@
+import React from 'react'
+export default class Application extends React.Component {
+  render() {
+    return <h2>This is my first react page from application!! </h2>
+  }
+}
