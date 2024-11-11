f1370808e83cf97d6e6dd1f0c2e1ad8c6d74fd24
JimmyCHL <a0930795105@gmail.com>
2024-11-11 11:39:42 -0500

    starting react

---
+++ ES6/react.js
@@ -0,0 +1,16 @@
+/**
+ * A task runner is a tool that automates repetitive tasks commonly involved in development workflows, particularly for web development.
+ * It helps with tasks like minification, compilation, testing, linting, deployment, and other processes that need to be executed regularly.
+ */
+
+// Library, for UI
+// Based on component based architecture
+// MVC - Model, View, Controller
+
+// Uses additional libraries for routing and data handling
+// Routing - React Router Dom <http:/>/localhost:9090/user/1981>
+// Data(Model) - Redux
+
+// Framework - not a framework as we have many dependencies
+
+//Component - (reusable components, to split at its minimal)
