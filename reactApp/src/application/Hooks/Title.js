74a741e6b778d1999984852b5633f18f300cfe70
JimmyCHL <a0930795105@gmail.com>
2025-02-06 11:28:37 -0500

    add class content

---
+++ reactApp/src/application/Hooks/Title.js
@@ -0,0 +1,7 @@
+import React from 'react'
+function Title() {
+  console.log(`rendering title`)
+  return <h2>Title - Understanding Performance Hooks</h2>
+}
+//export default Title;
+export default React.memo(Title)
