ed858882f2697c1b2c4858693b1229ed4c19dba4
JimmyCHL <a0930795105@gmail.com>
2025-02-07 11:29:03 -0500

    Add lazy load to reduce build size

--- reactApp/src/application/app.js
+++ reactApp/src/application/app.js
@@ -1,18 +1,6 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './app.css'
-
-//import UserComponent from "./ApplicationComponents/User/UserComponent.jsx"; //instead of component we are loading container
-import StudentContainer from './ApplicationComponents/Student/StudentContainer'
-// import UserContainer from './ApplicationComponents/User/UserContainer'
-// import UserHooks from './ApplicationComponents/User/UserHooksComponent'
-import { Cart } from './ApplicationComponents/Carts/Components/Cart.jsx'
-import { Checkout } from './ApplicationComponents/Checkout/components/Checkout.jsx'
-import { Coupon } from './ApplicationComponents/Coupon/components/Coupon.jsx'
-import { OrderList } from './ApplicationComponents/Order/components/OrderList.jsx'
-import { ProductForm } from './ApplicationComponents/Products/Components/ProductForm'
-import { ProductScreen } from './ApplicationComponents/Products/Components/ProductScreen'
-import { ReviewScreen } from './ApplicationComponents/Reviews/Components/ReviewScreen.jsx'
 import UserHooks from './ApplicationComponents/User/UserHooksRefComponent'
 import About from './CommonComponents/AboutComponent'
 import { AppCopy } from './CommonComponents/AppCopy'
@@ -25,6 +13,38 @@ import Home from './CommonComponents/HomeTrainerComponent'
 import NotFound from './CommonComponents/NotFoundComponent'
 import { TopicDetail, Topics } from './CommonComponents/TopicsComponent'
 
+//import UserComponent from "./ApplicationComponents/User/UserComponent.jsx"; //instead of component we are loading container
+const StudentContainer = lazy(() => import('./ApplicationComponents/Student/StudentContainer'))
+// import UserContainer from './ApplicationComponents/User/UserContainer'
+// import UserHooks from './ApplicationComponents/User/UserHooksComponent'
+const Cart = lazy(() =>
+  import('./ApplicationComponents/Carts/Components/Cart.jsx').then((module) => ({ default: module.Cart }))
+)
+const Checkout = lazy(() =>
+  import('./ApplicationComponents/Checkout/components/Checkout.jsx').then((module) => ({ default: module.Checkout }))
+)
+const Coupon = lazy(() =>
+  import('./ApplicationComponents/Coupon/components/Coupon.jsx').then((module) => ({ default: module.Coupon }))
+)
+const OrderList = lazy(() =>
+  import('./ApplicationComponents/Order/components/OrderList.jsx').then((module) => ({ default: module.OrderList }))
+)
+const ProductForm = lazy(() =>
+  import('./ApplicationComponents/Products/Components/ProductForm.jsx').then((module) => ({
+    default: module.ProductForm,
+  }))
+)
+const ProductScreen = lazy(() =>
+  import('./ApplicationComponents/Products/Components/ProductScreen.jsx').then((module) => ({
+    default: module.ProductScreen,
+  }))
+)
+
+const ReviewScreen = lazy(() =>
+  import('./ApplicationComponents/Reviews/Components/ReviewScreen.jsx').then((module) => ({
+    default: module.ReviewScreen,
+  }))
+)
 export default class ApplicationComponent extends React.Component {
   constructor(props) {
     super(props) //is used to passBack data <props -here> so that it is updated in base object for react framework
@@ -47,33 +67,35 @@ export default class ApplicationComponent extends React.Component {
     //switch - case works for router
     return (
       <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home user={this.state.user} />} />
-          <Route path="home" element={<Home />} />
-          <Route path="login" element={<UserHooks />} />
-          <Route path="studentLogin" element={<StudentContainer />} />
-          <Route path="/productForm" element={<ProductForm />} />
-          <Route path="/couponGeneration" element={<Coupon />} />
-          <Route path="/productsList" element={<ProductScreen />} />
-          <Route path="/review/:id" element={<ReviewScreen />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/orders" element={<OrderList />} />
-          <Route path="/cancelled-orders" element={<OrderList cancelledOrderOnly />} />
-          <Route path="/app" element={<AppCopy />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/atmDispenser" element={<ATMDispenser />} />
-          <Route path="/addHobby" element={<AddHobby />} />
-          <Route path="/about/:id" element={<About />} />
-          <Route path="/aws/image" element={<AW3Component />} />
-          <Route path="/topics/*" element={<Topics />}>
-            <Route path=":topicId" element={<TopicDetail />} />
-            <Route path="" element={<p>Please select a topic from the list.</p>} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home user={this.state.user} />} />
+            <Route path="home" element={<Home />} />
+            <Route path="login" element={<UserHooks />} />
+            <Route path="studentLogin" element={<StudentContainer />} />
+            <Route path="/productForm" element={<ProductForm />} />
+            <Route path="/couponGeneration" element={<Coupon />} />
+            <Route path="/productsList" element={<ProductScreen />} />
+            <Route path="/review/:id" element={<ReviewScreen />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/orders" element={<OrderList />} />
+            <Route path="/cancelled-orders" element={<OrderList cancelledOrderOnly />} />
+            <Route path="/app" element={<AppCopy />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/atmDispenser" element={<ATMDispenser />} />
+            <Route path="/addHobby" element={<AddHobby />} />
+            <Route path="/about/:id" element={<About />} />
+            <Route path="/aws/image" element={<AW3Component />} />
+            <Route path="/topics/*" element={<Topics />}>
+              <Route path=":topicId" element={<TopicDetail />} />
+              <Route path="" element={<p>Please select a topic from the list.</p>} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </Suspense>
       </Router>
     )
   }
